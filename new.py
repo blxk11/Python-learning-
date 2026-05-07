db52c24639d6d43a75b03856cc014974fa6412c9
blxk11 <vyshnavkv106@gmail.com>
2026-05-08 00:53:38 +0530

    new starting

--- new.py
+++ new.py
@@ -1,8 +1,4 @@
-nums = [3, 1, 4, 1, 5, 9, 7, 2, 6]
-count = 0
-re = 0
-for num in nums:
-    num = num + -1
-    count = (nums[-1]) + -1
-    
-print(count)
+name = "Alice"
+age = 22
+print(name)
+print(age)
