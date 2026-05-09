b9cc3017b985ff146322c29cf71d00527cb43032
blxk11 <vyshnavkv106@gmail.com>
2026-05-09 12:45:36 +0530

    basic practice

--- new.py
+++ new.py
@@ -1,2 +1,26 @@
-a = 45.6789
-print("%.2f" %a)
+#a = 45.6789
+#print("%.2f" %a)
+
+#a = "Hello, World!"
+#print(len(a))
+
+# a = "  too much space  "
+# print(a.strip())
+
+# a = "banana"
+# b = a.count("a")
+# print(b)
+
+# a = "Hello, World!"
+# print(a.upper())
+
+# a = 5
+# b = "apples"
+# print(f"I have {a} {b}")
+
+# a = "python programming"
+# print(a.capitalize())
+
+# a = "hello world"
+# b = a[::-1]
+# print(b.upper())
