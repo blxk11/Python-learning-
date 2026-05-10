1440526cd026a525c72dcc7ca9decdcaa9fe07df
blxk11 <vyshnavkv106@gmail.com>
2026-05-10 13:34:06 +0530

    FizzBuzz

--- level2.py
+++ level2.py
@@ -1,4 +1,18 @@
-num = 5
-for i in range(1 ,11):
-    mul = num * i
-    print(mul)
+# num = 5
+# for i in range(1 ,11):
+#     mul = num * i
+#     print(mul)
+
+num = 0
+for i in range(1, 51):
+    num += 1
+    
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else :
+        print(num)
+    
