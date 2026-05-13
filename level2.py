fce81604335671d45188968d6e4a0550e05d854a
blxk11 <vyshnavkv106@gmail.com>
2026-05-13 11:19:04 +0530

    while loop

--- level2.py
+++ level2.py
@@ -3,18 +3,23 @@
 #     mul = num * i
 #     print(mul)
 
-num = 0
-for i in range(1, 51):
-    num += 1
+# num = 0
+# for i in range(1, 51):
+#     num += 1
     
-    if num % 3 == 0 and num % 5 == 0:
-        print("FizzBuzz")
-    elif num % 3 == 0:
-        print("Fizz")
-    elif num % 5 == 0:
-        print("Buzz")
-    else :
-        print(num)
+#     if num % 3 == 0 and num % 5 == 0:
+#         print("FizzBuzz")
+#     elif num % 3 == 0:
+#         print("Fizz")
+#     elif num % 5 == 0:
+#         print("Buzz")
+#     else :
+#         print(num)
         
-print("keep going")
+# print("keep going")
+n = 10
+while n >=1:
+    print(n)
+    n = n - 1
+   
     
